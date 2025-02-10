80a7d46b2e2556913e597cf62d1b59b90b516f49
camilazci <camilazucchi@outlook.com>
2025-02-10 16:28:39 -0300

    feat: generate random number with Math.floor and parse user input with parseInt

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 // Passa uma mensagem para o usuário
 alert('Boas vindas ao jogo do número secreto!');
 
-const numeroSecreto = 4;
+const numeroSecreto = Math.floor(Math.random() * 10);
 let chute;
 let tentativas = 1;
 
 // Enquanto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = parseInt(prompt('Escolha um número entre 1 e 10'));
 
     if (numeroSecreto == chute) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas++;
     }
-    tentativas++;
 }
+
+// Operador ternário
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
