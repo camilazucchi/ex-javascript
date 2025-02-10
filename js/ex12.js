df51bc49c30561a0383ae43e80cb311b38046b90
camilazci <camilazucchi@outlook.com>
2025-02-10 16:46:46 -0300

    feat: add exercise 12

---
+++ js/ex12.js
@@ -0,0 +1,4 @@
+// Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir e mensagem "Olá, [seu nome]!" no console do navegador.
+const nome = 'Camila';
+
+console.log(`Olá, ${nome}!`);
