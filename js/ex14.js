36da238a772d2f33f0e63f4c9636302299810d80
camilazci <camilazucchi@outlook.com>
2025-02-10 16:53:09 -0300

    feat: add exercise 14

---
+++ js/ex14.js
@@ -0,0 +1,4 @@
+// Utilize o prompt e faça a seguinte pergunta: "Qual a linguagem de programação que você mais gosta?". Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+let linguagemProgramacao = prompt(`Qual a linguagem de programação que você mais gosta? :D`);
+
+console.log(`A linguagem de programação que o usuário mais gosta é ${linguagemProgramacao}!`);
