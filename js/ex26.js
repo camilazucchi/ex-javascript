f4fa82e1f422e0b52c6d782640c92fa88b2c8c4a
camilazci <camilazucchi@outlook.com>
2025-02-12 14:04:02 -0300

    feat: add exercise 26 (function with return)

---
+++ js/ex26.js
@@ -0,0 +1,7 @@
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function mostrarDobro(numero) {
+    return numero * 2;
+}
+
+const numero = mostrarDobro(10);
+console.log(numero);
