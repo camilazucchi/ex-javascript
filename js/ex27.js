b0cda359af295430b9221a1645255055ae2f563e
camilazci <camilazucchi@outlook.com>
2025-02-12 14:08:46 -0300

    feat: add exercise 27 (function with three parameters and return value)

---
+++ js/ex27.js
@@ -0,0 +1,8 @@
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function media(num1, num2, num3) {
+    const sum = num1 + num2 + num3;
+    return sum / 3;
+}
+
+const resultado = media(10, 10, 10);
+console.log(resultado);
