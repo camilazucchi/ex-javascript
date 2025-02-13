15576d07c5145f62e16451390500697a71d5bad2
camilazci <camilazucchi@outlook.com>
2025-02-13 15:35:38 -0300

    feat: add arrays exercises

---
+++ js/ex36.js
@@ -0,0 +1,23 @@
+// 1. Crie uma lista vazia, com o nome listaGenerica.
+const listaGenerica = [];
+
+// 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' e 'Python'.
+const linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+// Exibe a lista linguagensDeProgramacao
+console.log(linguagensDeProgramacao);
+
+// 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+const nomes = ['Sakura', 'Hinata', 'Naruto'];
+console.log(nomes[0]);
+
+// 5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(nomes[1]);
+
+// 6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(nomes[2]);
